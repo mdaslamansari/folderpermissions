1fee16500f6a479752240954e8a68da557fbaa7d
aslam <maansari@bechtel.com>
2017-07-05 20:38:53 +0530

    code

---
+++ buildtask/FolderPermissions.ps1
@@ -0,0 +1,45 @@
+﻿param (
+        [string]$machinesList,
+        [string]$AdminUserName,
+        [string]$AdminPassword,
+        [string]$Path,
+        [string]$Users,
+        [string]$Control     
+        )
+
+Write-Output "Machine Name - $machinesList"
+Write-Output "Admin User Name - $AdminUserName"
+Write-Output "Folder Location - $Path"
+Write-Output "User List - $Users"
+Write-Output "Type of access - $Control"
+
+$pass = ConvertTo-SecureString -AsPlainText $AdminPassword -Force
+
+$credential = new-object -typename System.Management.Automation.PSCredential -argumentlist $AdminUserName,$pass
+
+Invoke-Command -ComputerName $machinesList -credential $credential  -ScriptBlock {
+
+param (
+        [string]$Path,
+        [string]$Users,
+        [string]$Control          
+        )
+
+Write-Output "Folder Location - $Path"
+Write-Output "User List - $Users"
+Write-Output "Type of access - $Control"
+
+$UserSplit = $Users -split ";"
+$FolderSplit = $Path -split ";"
+
+foreach ($Folder in $FolderSplit)
+{
+    ForEach ($User in $UserSplit)
+    {
+        $Acl = Get-Acl $Folder
+        $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule($User,$Control,"ContainerInherit,ObjectInherit","None","Allow")
+        $Acl.SetAccessRule($Ar)
+        Set-Acl $Folder $Acl
+    }
+}
+} -ArgumentList ($Path, $Users, $Control)
