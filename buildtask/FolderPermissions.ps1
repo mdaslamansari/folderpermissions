e6e2ea2e1cb22909b616b8ae2535d21dc8ac3238
baileyjs78 <jbailey@northernsafety.com>
2018-10-19 14:20:20 -0400

    Add code to create session based on WinRM protocol

--- buildtask/FolderPermissions.ps1
+++ buildtask/FolderPermissions.ps1
@@ -2,6 +2,7 @@
         [string]$machinesList,
         [string]$AdminUserName,
         [string]$AdminPassword,
+        [string]$remoteWinRMProtocol,
         [string]$Path,
         [string]$Users,
         [string]$Control     
@@ -9,6 +10,7 @@
 
 Write-Output "Machine Name - $machinesList"
 Write-Output "Admin User Name - $AdminUserName"
+Write-Host "Remote WinRM protocol - $($remoteWinRMProtocol)"
 Write-Output "Folder Location - $Path"
 Write-Output "User List - $Users"
 Write-Output "Type of access - $Control"
@@ -17,29 +19,39 @@ $pass = ConvertTo-SecureString -AsPlainText $AdminPassword -Force
 
 $credential = new-object -typename System.Management.Automation.PSCredential -argumentlist $AdminUserName,$pass
 
-Invoke-Command -ComputerName $machinesList -credential $credential  -ScriptBlock {
-
-param (
-        [string]$Path,
-        [string]$Users,
-        [string]$Control          
-        )
-
-Write-Output "Folder Location - $Path"
-Write-Output "User List - $Users"
-Write-Output "Type of access - $Control"
-
-$UserSplit = $Users -split ";"
-$FolderSplit = $Path -split ";"
-
-foreach ($Folder in $FolderSplit)
+if ($remoteWinRMProtocol -eq "HTTPS")
 {
-    ForEach ($User in $UserSplit)
-    {
-        $Acl = Get-Acl $Folder
-        $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule($User,$Control,"ContainerInherit,ObjectInherit","None","Allow")
-        $Acl.SetAccessRule($Ar)
-        Set-Acl $Folder $Acl
-    }
+    Write-Host "Connecting to remote machine [$($machinesList)] using SSL..."
+    $session = New-PSSession -ComputerName $machinesList -Credential $credential -UseSSL -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
 }
+else 
+{
+    Write-Host "Connecting to remote machine [$($machinesList)]..."
+    $session = New-PSSession -ComputerName $machinesList -Credential $credential -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
+} 
+
+Invoke-Command -Session $session -ScriptBlock {
+        param (
+                [string]$Path,
+                [string]$Users,
+                [string]$Control          
+                )
+
+        Write-Output "Folder Location - $Path"
+        Write-Output "User List - $Users"
+        Write-Output "Type of access - $Control"
+
+        $UserSplit = $Users -split ";"
+        $FolderSplit = $Path -split ";"
+
+        foreach ($Folder in $FolderSplit)
+        {
+            ForEach ($User in $UserSplit)
+            {
+                $Acl = Get-Acl $Folder
+                $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule($User,$Control,"ContainerInherit,ObjectInherit","None","Allow")
+                $Acl.SetAccessRule($Ar)
+                Set-Acl $Folder $Acl
+            }
+        }
 } -ArgumentList ($Path, $Users, $Control)
